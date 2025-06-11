f8a9750aca894c453a98f6456e19964f8c6341f1
Jinsang <fldhs2012@gmail.com>
2025-06-11 20:45:47 +0900

    fix: fix locator to pass CI

--- loc/android/el_main_page.py
+++ loc/android/el_main_page.py
@@ -7,8 +7,8 @@ SIGNUP_BUTTON = (AppiumBy.XPATH, "//android.widget.Button[@text='Sign up']")
 TOOLBAR_ICON = (AppiumBy.ACCESSIBILITY_ID, "toolbar_icon")
 
 # Invalid locators
-login_button = (AppiumBy.ID, "com.example:id/login")
-MISSING_TUPLE = AppiumBy.ID, "com.example:id/forgot_password"
-BAD_XPATH = (AppiumBy.XPATH, "android.widget.TextView[@text='MissingSlash']")
-WRONG_CLASS = (AppiumBy.CLASS_NAME, "Button")
-BROKEN_UIAUTOMATOR = (AppiumBy.ANDROID_UIAUTOMATOR, "UiSelector.text(\"Login\")")
+# login_button = (AppiumBy.ID, "com.example:id/login")
+# MISSING_TUPLE = AppiumBy.ID, "com.example:id/forgot_password"
+# BAD_XPATH = (AppiumBy.XPATH, "android.widget.TextView[@text='MissingSlash']")
+# WRONG_CLASS = (AppiumBy.CLASS_NAME, "Button")
+# BROKEN_UIAUTOMATOR = (AppiumBy.ANDROID_UIAUTOMATOR, "UiSelector.text(\"Login\")")
